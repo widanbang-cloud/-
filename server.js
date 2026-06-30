98e3728a21d94b69fd3d72e87d474a88a1c7ea76
widanbang-cloud <widanbang@gmail.com>
2026-06-30 17:56:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ app.use(cors());
 
 // Gmail 앱 비밀번호로 발송
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: 'orangesho123@gmail.com',
     pass: 'vqezwmlcqxsgsgwb'
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 // 이메일 발송 API
